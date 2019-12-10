8f851858925ac054621db90d2129a47edb7fecac
ling <846121817@qq.com>
2019-12-10 17:31:52 +0800

    单数据源

--- data-source/src/main/java/com/cypher/datasource/DataSourceApplication.java
+++ data-source/src/main/java/com/cypher/datasource/DataSourceApplication.java
@@ -1,13 +1,41 @@
-package com.cypher.datasource;
+package com.maple.datasource;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @SpringBootApplication
-public class DataSourceApplication {
+@Slf4j
+public class DataSourceApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(DataSourceApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        showConnection();
+    }
+
+    private void showConnection() {
+        try {
+            log.info(dataSource.toString());
+            Connection conn = dataSource.getConnection();
+            log.info(conn.toString());
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
