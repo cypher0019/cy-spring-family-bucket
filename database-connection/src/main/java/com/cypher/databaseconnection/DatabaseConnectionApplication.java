eb0b645c9fa50b819763a5b3a348a1da9e34a09f
cypher0019 <lf846121817@163.com>
2019-12-23 22:52:19 +0800

    数据库连接初始化

---
+++ database-connection/src/main/java/com/cypher/databaseconnection/DatabaseConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.cypher.databaseconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseConnectionApplication.class, args);
+    }
+
+}
---
+++ database-connection/src/test/java/com/cypher/databaseconnection/DatabaseConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cypher.databaseconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseConnectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
